98309090a0deeb137cc9650b077b215d34a63e6c
YuviPanda <yuvipanda@gmail.com>
2026-02-01 12:52:19 -0800

    Enable debug logging in dokku

--- dokku.bash
+++ dokku.bash
@@ -3,6 +3,7 @@ set -euo pipefail
 # Dokku entrypoint, to make life easier
 
 exec python3 -m jupyterbook_pub.app \
+    --JupyterBookPubApp.debug=true \
     --JupyterBookPubApp.port=${PORT} \
     --JupyterBookPubApp.built_sites_root=/opt/persistent/sites \
     --JupyterBookPubApp.repo_checkout_root=/opt/persistent/repos
