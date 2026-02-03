3faf3e96bb67cf7d832d1ad4ea9290231acfa537
YuviPanda <yuvipanda@gmail.com>
2026-02-03 10:28:26 -0800

    Update header

--- js/src/App.tsx
+++ js/src/App.tsx
@@ -8,7 +8,8 @@ export function App() {
         <div className='mx-auto col-8'>
           <div className='text-center mt-4'>
             <h1>JupyterBook.pub (name tbd)</h1>
-            <h4>Instantly build and share your JupyterBook repository from GitHub or Dataverse</h4>
+            <h5>Instantly build and share your JupyterBook repository from GitHub or Dataverse or Zenodo</h5>
+            <a href='https://github.com/yuvipanda/jupyterbook.pub/issues'>File Issues</a>
           </div>
           <LinkGenerator />
         </div>
