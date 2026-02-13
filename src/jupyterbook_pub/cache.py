32a680439bfb683ac425aa810f186dca0ec49c08
YuviPanda <yuvipanda@gmail.com>
2026-02-13 10:49:51 -0800

    Encode JSON well

--- src/jupyterbook_pub/cache.py
+++ src/jupyterbook_pub/cache.py
@@ -10,17 +10,17 @@ import json
 from base64 import urlsafe_b64encode
 
 from repoproviders.resolvers.base import MaybeExists, Repo
-from repoproviders.resolvers.serialize import to_dict
+from repoproviders.resolvers.serialize import to_dict, JSONEncoder
 
 
 def make_rendered_cache_key(repo: Repo, base_url: str) -> str:
     answer = MaybeExists(repo)
     key = {"answer": to_dict(answer), "base_url": base_url}
-    return urlsafe_b64encode(hashlib.sha256(json.dumps(key).encode()).digest()).decode()
+    return urlsafe_b64encode(hashlib.sha256(json.dumps(key, cls=JSONEncoder).encode()).digest()).decode()
 
 
 def make_checkout_cache_key(repo: Repo) -> str:
     answer = MaybeExists(repo)
     return urlsafe_b64encode(
-        hashlib.sha256(json.dumps(to_dict(answer)).encode()).digest()
+        hashlib.sha256(json.dumps(to_dict(answer), cls=JSONEncoder).encode()).digest()
     ).decode()
