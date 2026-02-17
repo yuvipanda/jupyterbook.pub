9335827cbc64e651784ef33eb94bfbb2748b970d
YuviPanda <yuvipanda@gmail.com>
2026-02-17 13:25:43 -0800

    Allow toggling config file with env var

--- dokku.bash
+++ dokku.bash
@@ -2,8 +2,11 @@
 set -euo pipefail
 # Dokku entrypoint, to make life easier
 
+CONFIG_FILE="deploy_config/{$CONFIG_FILE:-jupyterbook.py}"
+
 exec python3 -m jupyterbook_pub.app \
     --JupyterBookPubApp.debug=true \
     --JupyterBookPubApp.port=${PORT} \
     --JupyterBookPubApp.built_sites_root=/opt/persistent/sites \
-    --JupyterBookPubApp.repo_checkout_root=/opt/persistent/repos
+    --JupyterBookPubApp.repo_checkout_root=/opt/persistent/repos \
+    -f ${CONFIG_FILE}
