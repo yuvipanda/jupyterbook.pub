ec8dd260923a5d2ac23242a4584379fc57301ab7
YuviPanda <yuvipanda@gmail.com>
2026-02-17 12:20:55 -0800

    Remove leftover copy paste comment

--- src/jupyterbook_pub/builder/lite.py
+++ src/jupyterbook_pub/builder/lite.py
@@ -7,8 +7,6 @@ from jupyterbook_pub.builder.base import Renderer
 class JupyterLiteBuilder(Renderer):
     async def render(self, repo_path: Path, built_path: Path, base_url: str):
         if not built_path.exists():
-            # Explicitly pass in a random port, as otherwise jupyter-book will always
-            # try to listen on port 5000 and hang forever if it can't.
             command = [
                 "jupyter",
                 "lite",
