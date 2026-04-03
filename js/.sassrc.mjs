b9cff7b105981a0831e027a75f12fcc34c8112d6
Angus Hollands <goosey15@gmail.com>
2026-04-03 11:45:41 +0100

    fix: remove unused file

--- js/.sassrc.mjs
+++
@@ -1,7 +0,0 @@
-import sass from "sass";
-const config = {
-    silenceDeprecations: ["import", "mixed-decls", "color-functions", "global-builtin"],
-    importers: [new sass.NodePackageImporter()],
-};
-
-export default config;
