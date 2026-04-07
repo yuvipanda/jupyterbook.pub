6949ce3bb2dd341014371c3c0ffbea9de2fc3d23
Angus Hollands <goosey15@gmail.com>
2026-04-07 09:28:26 +0100

    fix: set xsrf in fetch

--- js/src/resolver.ts
+++ js/src/resolver.ts
@@ -12,6 +12,13 @@ export async function resolve(
     const queryUrl = new URL(`${baseUrl}api/v1/resolve`, window.location.href);
     queryUrl.searchParams.append("q", question);
 
+    const cookie = document.cookie;
+
+    const xsrfTokenMatch = cookie.match('\\b_xsrf=([^;]*)\\b');
+    if (xsrfTokenMatch) {
+      queryUrl.searchParams.append('_xsrf', xsrfTokenMatch[1]);
+    }
+
     const resp = await fetch(queryUrl);
     if (!resp.ok) {
         return null;
