ed90b52c38546b6b049182cff98f988898316d57
Angus Hollands <goosey15@gmail.com>
2026-05-06 15:17:53 +0100

    refactor: use single storage root
    This simplifies things, and the separation is not currently used.

--- dokku.bash
+++ dokku.bash
@@ -5,8 +5,7 @@ set -euo pipefail
 CONFIG_FILE="deploy_config/${CONFIG_FILE:-jupyterbook.py}"
 
 exec python3 -m jupyterbook_pub.app \
-    --JupyterBookPubApp.debug=true \
-    --JupyterBookPubApp.port=${PORT} \
-    --JupyterBookPubApp.built_sites_root=/opt/persistent/sites \
-    --JupyterBookPubApp.repo_checkout_root=/opt/persistent/repos \
+    --debug \
+    --port=${PORT} \
+    --storage=/opt/persistent/ \
     -f ${CONFIG_FILE}
